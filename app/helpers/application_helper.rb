37265332d593a56dcee31fba861817c03cff0f1f
codefalcon <codefalcon@server.fake>
2013-08-01 00:46:36 +0530

    initial project version

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,16 @@
+module ApplicationHelper
+  class Status
+    @@Active = 1;
+    @@Inactive = 0;
+  end
+  class Role
+    @@Admin = 1;
+    @@DonorTeam = 2;
+    @@Donor = 3;
+    @@ProjectDirector = 4;
+    @@ProjectManager = 5;
+    @@BlockTrainer = 6;
+    @@TrainingMonitor = 7;
+    @@Teacher = 8;
+  end
+end
---
+++ app/views/districts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @district, :districtIdentifier, :name, :status, :created_at, :updated_at
