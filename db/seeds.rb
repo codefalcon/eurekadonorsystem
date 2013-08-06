d08383778c395860e9e72c2723069b9ad38898e5
gayathrisri23 <gayathri@server.fake>
2013-08-06 21:53:30 +0530

    Remove column from index page; Add default donor user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user = User.find_by_email_address('info@aidindia.in')
+
+if(@user.nil?)
+User.create!(:first_name => 'Donor', :last_name => 'Team', :email_address => 'info@aidindia.in', :phone_number => '+91-44-28602308', :gender => 0, :login => 'donor_team', :password => '!Password', :password_confirmation => '!Password', :role => 1, :status => 1)
+end
