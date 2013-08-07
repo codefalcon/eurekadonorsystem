3cb7145680b3f0b0417dab43ea3e06029d9f31ec
gayathrisri23 <gayathri@server.fake>
2013-08-07 20:55:12 +0530

    District UI in popup

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,11 +1,12 @@
 class DistrictsController < ApplicationController
   before_filter :login_required
   before_action :set_district, only: [:show, :edit, :update, :destroy]
+  include ApplicationHelper
 
   # GET /districts
   # GET /districts.json
   def index
-    @districts = District.all
+    @districts = District.find_all_by_status(Status::Active)
   end
 
   # GET /districts/1
@@ -27,13 +28,17 @@ class DistrictsController < ApplicationController
   def create
     @district = District.new(district_params)
 
+    @district.status = Status::Active
+
     respond_to do |format|
       if @district.save
-        format.html { redirect_to @district, notice: 'District was successfully created.' }
+        format.html { redirect_to districts_path, notice: 'District was successfully created.' }
+	format.js { render :js => "close_modal();refresh_page();" }
         format.json { render action: 'show', status: :created, location: @district }
       else
         format.html { render action: 'new' }
         format.json { render json: @district.errors, status: :unprocessable_entity }
+	format.js
       end
     end
   end
@@ -41,13 +46,17 @@ class DistrictsController < ApplicationController
   # PATCH/PUT /districts/1
   # PATCH/PUT /districts/1.json
   def update
+    @district.status = Status::Active
+
     respond_to do |format|
       if @district.update(district_params)
         format.html { redirect_to @district, notice: 'District was successfully updated.' }
+	format.js { render :js => "close_modal();refresh_page();" }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
         format.json { render json: @district.errors, status: :unprocessable_entity }
+	format.js
       end
     end
   end
@@ -55,7 +64,10 @@ class DistrictsController < ApplicationController
   # DELETE /districts/1
   # DELETE /districts/1.json
   def destroy
-    @district.destroy
+    @user.status = Status::Inactive
+
+    @user.save
+
     respond_to do |format|
       format.html { redirect_to districts_url }
       format.json { head :no_content }
@@ -70,6 +82,6 @@ class DistrictsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def district_params
-      params.require(:district).permit(:name, :status)
+      params.require(:district).permit(:name, :district_code)
     end
 end
